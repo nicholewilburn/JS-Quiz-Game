7522dea5aeb067042ea03c9c7ec67d0a1955d4d8
nicho <nicholewilburn@gmail.com>
2023-05-24 16:02:52 -0500

    all of the basic game logic implemented, needs results and scores

--- Assets/questions.js
+++ Assets/questions.js
@@ -1,9 +1,24 @@
-var questionList = {
+var questionList = [
 
-    question1 : {
+    {
         question : "What is not a JS data type?",
         choices : ['pickles','strings','boolean','integers'],
         answer : 'pickles'
-    }
+    },
+    {
+        question : "What is not something you use along with JS to create great websites?",
+        choices : ['HTML','JQuery','Minecraft','CSS'],
+        answer : 'Minecraft'
+    },
+    {
+        question : "Which is not considered a type of scope in JS?",
+        choices : ['global','stetho','module','function'],
+        answer : 'stetho'
+    },
+    {
+        question : "How would you write something to the console?",
+        choices : ['console.log("message")','write("message")','press a button','with pen and paper duh'],
+        answer : 'console.log("message")'
+    },
 
-}
+]
